6567cb35cdec4c3045f0555a74586470e59f3c5f
gaganjangid73 <gaganjangid02@gmail.com>
2025-08-04 15:49:35 +0530

    inside server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const http = require("http");
 
-const Server = http.createServer();
+const Server = http.createServer(function (req, res){
+
+    if(req.url==="/getdata"){
+        res.end("there are no data..❤️")
+    }
+    res.end("hello world, my name is gagan ");
+});
 
 Server.listen(7777);
