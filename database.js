6d0736f9d8c248c630cc2ad64ae8182217f10165
gaganjangid73 <gaganjangid02@gmail.com>
2025-08-31 15:10:31 +0530

    some changes

--- database.js
+++ database.js
@@ -1,4 +1,9 @@
-const URI = "mongodb+srv://GaganJangid:<Gagan123>@devtinder.jmz59y2.mongodb.net"
+const {MongoClient} = require("mongodb")
+
+const url = "mongodb+srv://GaganJangid:<Gagan123>@devtinder.jmz59y2.mongodb.net"
+
+const client = new MongoClient(url);
+const dbName = 'helloworld';
 
 
 // Gagan@02
